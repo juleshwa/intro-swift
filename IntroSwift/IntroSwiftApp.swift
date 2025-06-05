56ed5ff43650ee9525a998b530e5cdf582fb2ac3
Juli Yanti <juleshwa@gmail.com>
2025-06-06 03:55:09 +0700

    Initial Commit

---
+++ IntroSwift/IntroSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSwiftApp.swift
+//  IntroSwift
+//
+//  Created by Juli Yanti on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct IntroSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
